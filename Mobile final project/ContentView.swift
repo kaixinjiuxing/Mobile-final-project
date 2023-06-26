39596bf965e574acba6fa4f323da22da1431ab5f
kaixinjiuxing <hanaruihanliu@gmail.com>
2023-06-26 10:49:18 -0700

    Initial Commit

---
+++ Mobile final project/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Mobile final project
+//
+//  Created by Ruihan Liu on 6/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mobile final project/Mobile_final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mobile_final_projectApp.swift
+//  Mobile final project
+//
+//  Created by Ruihan Liu on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Mobile_final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
