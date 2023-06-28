1fc5dbe6648425f1fe6e38f39257399c429d4a9e
MiaYGithub <miagraceyee@gmail.com>
2023-06-28 13:57:01 -0700

    added everything on SwiftUIView

--- Mobile final project/SwiftUIView.swift
+++ Mobile final project/SwiftUIView.swift
@@ -10,17 +10,31 @@ import SwiftUI
 struct SwiftUIView: View {
     
     var body: some View {
-        VStack {
+        
+        
+        VStack(alignment: .leading) {
             Text("Recipes to Try: ")
                 .font(.title)
+                .fontWeight(.semibold)
+                .fontWeight(.medium)
                 .multilineTextAlignment(.leading)
+                .padding(.leading, 23.0)
                 
+           
             TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
-                .padding(.all, 100.0)
+                .padding(.all)
                 .multilineTextAlignment(.center)
                 .border(Color(red: 0.929, green: 0.662, blue: 0.773), width: 3)
             
+            Text("My Recipes:")
+                .font(.title)
+                .fontWeight(.semibold)
+                .multilineTextAlignment(.leading)
+                .padding(.leading, 23.0)
+            
+            
         }
+        .padding(.all)
     }
 }
 
