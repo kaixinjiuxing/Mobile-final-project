0372adcc73d21c71c5782269cf6fd0b352cc4343
MiaYGithub <miagraceyee@gmail.com>
2023-06-28 14:55:08 -0700

    Colors changed, text boxes added

--- Mobile final project/SwiftUIView.swift
+++ Mobile final project/SwiftUIView.swift
@@ -10,33 +10,45 @@ import SwiftUI
 struct SwiftUIView: View {
     
     var body: some View {
-        
-        ZStack {
-            Color(red: 0.929,green: 0.862, blue: 0.873)
-            VStack(alignment: .leading) {
-                
-                Text("Recipes to Try: ")
-                    .font(.title)
-                    .fontWeight(.semibold)
-                    .fontWeight(.medium)
-                    .multilineTextAlignment(.leading)
-                    .padding(.leading, 23.0)
-                
-                
-                TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
-                    .padding(.all)
-                    .multilineTextAlignment(.center)
-                    .border(Color(red: 0.929, green: 0.662, blue: 0.773), width: 3)
-                
-                Text("My Recipes:")
-                    .font(.title)
-                    .fontWeight(.semibold)
-                    .multilineTextAlignment(.leading)
-                    .padding(.leading, 23.0)
-                
-                
+        NavigationStack {
+            
+            ZStack {
+                Color(red: 0.929,green: 0.862, blue: 0.873)
+                VStack(alignment: .leading) {
+                    
+                    Text("Recipes to Try: ")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .fontWeight(.medium)
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading, 23.0)
+                    
+                    
+                    TextField("Type or insert recipe links here!", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                        .padding(.all, 100.0)
+                        .multilineTextAlignment(.center)
+                        .border(Color(red: 0.929, green: 0.662, blue: 0.773), width: 3)
+                        
+                    
+                    Text("My Recipes:")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading, 23.0)
+                    
+                    TextField("Type or insert recipe links here!", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                        .padding(.all, 100.0)
+                        .multilineTextAlignment(.center)
+                        .border(Color(red: 0.929, green: 0.662, blue: 0.773), width: 3)
+                    
+                }
+                .padding(.all)
+                .navigationTitle("Recipe Journal")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbarBackground(Color(red: 0.929, green: 0.662, blue: 0.773), for: .navigationBar)
+                .toolbarBackground(.visible, for: .navigationBar)
+                .bold()
             }
-            .padding(.all)
         }
     }
 }
