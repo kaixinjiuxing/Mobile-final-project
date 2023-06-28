cf2bb48805d59893a8f96b4091752e4cddea2013
kaixinjiuxing <hanaruihanliu@gmail.com>
2023-06-28 14:19:08 -0700

    main screen, will work on it more tonight

--- Mobile final project/ContentView.swift
+++ Mobile final project/ContentView.swift
@@ -9,12 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello!")
-            Text("KWK")
+       
+        NavigationStack {
+            VStack {
+                Text("ReceipEats")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                Image("Map")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                VStack(alignment: .leading, spacing: 35.0){
+                    VStack(alignment: .leading, spacing: 1.0){
+                        Text("Hello!")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                      
+                        Text("Explore the different types of food from different cultures by clicking the pins on the map above")
+                                .font(.subheadline)
+                                .fontWeight(.bold)
+                    }
+                    
+                        
+                    VStack(alignment: .leading, spacing:60.0){
+                        NavigationLink(destination: SwiftUIView()) {
+                            Text("My Recipes")
+                        }
+                        NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                            Text("Recipes to Try")
+                        }
+                    }
+                }
+            }
         }
         .padding()
     }
