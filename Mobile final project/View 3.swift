b04d18dcef2ef210b9ff87e8169e2dc9a2643a57
MiaYGithub <miagraceyee@gmail.com>
2023-06-28 13:05:56 -0700

    New views added

---
+++ Mobile final project/View 3.swift
@@ -0,0 +1,20 @@
+//
+//  View 3.swift
+//  Mobile final project
+//
+//  Created by Mia Yee on 6/28/23.
+//
+
+import SwiftUI
+
+struct View_3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct View_3_Previews: PreviewProvider {
+    static var previews: some View {
+        View_3()
+    }
+}
