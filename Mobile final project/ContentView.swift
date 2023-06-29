70373b5987745086db26be52c2f6b31dd4e942de
kaixinjiuxing <hanaruihanliu@gmail.com>
2023-06-29 11:50:18 -0700

    More pins added to the map

--- Mobile final project/ContentView.swift
+++ Mobile final project/ContentView.swift
@@ -9,108 +9,146 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationStack {
-            ZStack {
-                        Color(.systemPink)
-                            .ignoresSafeArea()
-                VStack(spacing: 25.0) {
-                    Spacer()
-                    Text("ReceipEats")
-                        .font(.largeTitle)
-                        .fontWeight(.heavy)
-                        .multilineTextAlignment(.center)
-                    
-                //NavigationStack{
-                    ZStack{
-                        Image("Map")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            //.frame(width: 350, height:230)
+            NavigationStack {
+                ZStack {
+                    Color(.systemPink)
+                        .ignoresSafeArea()
+                    VStack(spacing: 25.0) {
+                        Spacer()
+                        Text("ReceipEats")
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .multilineTextAlignment(.center)
                         
-                        NavigationLink(destination: View_3()) {
-                            Image("Pin")
+                        ZStack{
+                            Image("Map")
                                 .resizable()
-                                .frame(width:20)
-                                .frame(height:20)
-                                .offset(x:10, y:10)
-                        }
-                        
-                    }
-                //}
-                    
-                    ZStack{
-                        VStack(alignment: .leading){
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 350, height: 230)
                             
-                            VStack(alignment: .leading){
-                                Text("Hello!")
-                                    .font(.largeTitle)
-                                
-                                    .fontWeight(.bold)
-                                
-                                Text("Explore the different types of food from different cultures by clicking the pins on the map above")
-                                    .font(.subheadline)
-                                    .fontWeight(.bold)
+                            NavigationLink(destination: View_3()) {
+                                Image("Pin")
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
                             }
-                            Spacer()
-                            VStack(alignment: .leading, spacing:60.0){
-                                NavigationLink(destination: SwiftUIView()) {
-                                    HStack{
-                                        Text("My Recipes")
-                                        
-                                            .padding()
-                                            .foregroundColor(Color.white)
-                                            .font(.headline)
-                                        Spacer()
-                                    }
-                                    .background(Color.pink)
-                                    .cornerRadius(10)
-                                    .frame(width: 330)
-                                    
-                                }
-                                NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
-                                    HStack{
-                                        Text("Recipes to try")
-                                        
-                                            .padding()
-                                            .foregroundColor(Color.white)
-                                            .font(.headline)
-                                        Spacer()
-                                    }
-                                    .background(Color.pink)
-                                    .cornerRadius(10)
-                                    .frame(width: 330)
-                                }
+                            .position(x:-37, y:15)
+                            .frame(width: 20, height: 20)
+                            
+                            NavigationLink(destination: View_3()) {
+                                Image("Pin")
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
                             }
-                            Spacer()
+                            .position(x:-92, y:-38)
+                            .frame(width: 20, height: 20)
+                            
+                            NavigationLink(destination: View_3()) {
+                                Image("Pin")
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                            }
+                            .position(x:14, y:-19)
+                            .frame(width: 20, height: 20)
+                            
+                            NavigationLink(destination: View_3()) {
+                                Image("Pin")
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                            }
+                            .position(x:10, y:-60)
+                            .frame(width: 20, height: 20)
+                            
+                            NavigationLink(destination: View_3()) {
+                                Image("Pin")
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                            }
+                            .position(x:120, y:30)
+                            .frame(width: 20, height: 20)
+                            
+                            NavigationLink(destination: View_3()) {
+                                Image("Pin")
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                            }
+                            .position(x:100, y:-37)
+                            .frame(width: 20, height: 20)
                         }
-                        .padding(10)
-                        .background(Rectangle()
-                            .foregroundColor(.white))
-                        .cornerRadius(15)
-                        .shadow(radius: 15)
                         
                         
+                        ZStack{
+                            VStack(alignment: .leading){
+                                
+                                VStack(alignment: .leading){
+                                    Text("Hello!")
+                                        .font(.largeTitle)
+                                    
+                                        .fontWeight(.bold)
+                                    
+                                    Text("Explore the different types of food from different cultures by clicking the pins on the map above")
+                                        .font(.subheadline)
+                                        .fontWeight(.bold)
+                                }
+                                Spacer()
+                                
+                                VStack(alignment: .leading, spacing:60.0){
+                                    NavigationLink(destination: SwiftUIView()) {
+                                        HStack{
+                                            Text("My Recipes")
+                                            
+                                                .padding()
+                                                .foregroundColor(Color.white)
+                                                .font(.headline)
+                                            Spacer()
+                                        }
+                                        .background(Color.pink)
+                                        .cornerRadius(10)
+                                        .frame(width: 330)
+                                        
+                                    }
+                                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                                        HStack{
+                                            Text("Recipes to try")
+                                            
+                                                .padding()
+                                                .foregroundColor(Color.white)
+                                                .font(.headline)
+                                            Spacer()
+                                        }
+                                        .background(Color.pink)
+                                        .cornerRadius(10)
+                                        .frame(width: 330)
+                                    }
+                                }
+                                
+                                Spacer()
+                            }
+                            .padding(10)
+                            .background(Rectangle()
+                                .foregroundColor(.white))
+                            .cornerRadius(15)
+                            .shadow(radius: 15)
+                            
+                            
+                        }
+                        Spacer()
                     }
-                    Spacer()
-                }
-                .padding(.bottom, -25)
-                        .padding(5)
-                                .background(Rectangle()
-                                    .foregroundColor(.white))
-                                        .cornerRadius(15)
-                                        .shadow(radius: 15)
-                                .padding()
-                
-                
-                
-            }
+                    .padding(.bottom, -25)
+                    .padding(5)
+                    .background(Rectangle()
+                        .foregroundColor(.white))
+                    .cornerRadius(15)
+                    .shadow(radius: 15)
+                    .padding()
+                    
+             }
+         }
+    }
+}
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
         }
     }
-}
 
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
