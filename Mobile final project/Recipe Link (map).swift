4420046ace9ab08e2a0e555cf0162f253ff0aebb
MiaYGithub <miagraceyee@gmail.com>
2023-06-29 13:28:27 -0700

    Pad Thai view

--- Mobile final project/Recipe Link (map).swift
+++ Mobile final project/Recipe Link (map).swift
@@ -23,14 +23,30 @@ struct Recipe_Link__map_: View {
                     .font(.title)
                     .fontWeight(.semibold)
                     .multilineTextAlignment(.leading)
-                Text("Pad Thai is a stirfry rice noodle dish that originated in Thailand during the 20th century. ")
+                Text("Pad Thai is a stirfry rice noodle dish that originated in Thailand during the 20th century. In this dish there are delicious flavors of tamarid and fish sauce, and it is usually topped with shrimp, peanuts, cilantro, and lime wedges!")
+                
+                Image("Screenshot 2023-06-29 at 1.04.44 PM")
+                    .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0), resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                Link("NY Times Pad Thai Recipe", destination: URL(string: "https://cooking.nytimes.com/recipes/1013121-pad-thai")! )
+                    .environment(\.openURL, OpenURLAction { url in
+                        print("Open \(url)")
+                        return .handled
+                    })
+                    
+                   
             }
-            .padding(/*@START_MENU_TOKEN@*/.all, 24.0/*@END_MENU_TOKEN@*/)
-           
+            .padding(/*@START_MENU_TOKEN@*/.all, 28.0/*@END_MENU_TOKEN@*/)
+                    
+                    
+                
+            }
+            
+            
         
         }
     }
-    }
+
 
 
 struct Recipe_Link__map__Previews: PreviewProvider {
