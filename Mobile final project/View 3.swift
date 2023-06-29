d0d2053f3b1cf7478e103a43438307a6bcbce280
jcriqui <jasminecriqui@gmail.com>
2023-06-29 09:37:12 -0700

    journal attempt 1

--- Mobile final project/View 3.swift
+++ Mobile final project/View 3.swift
@@ -4,17 +4,30 @@
 //
 //  Created by Mia Yee on 6/28/23.
 //
-
 import SwiftUI
-
 struct View_3: View {
+    @State private var ingredients = "Add Ingredients"
+    @State private var steps = "Add Steps"
+    @State private var chefsnotes = "Add Notes"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct View_3_Previews: PreviewProvider {
-    static var previews: some View {
-        View_3()
-    }
-}
+        NavigationStack {
+            TextEditor (text: $ingredients)
+                .foregroundStyle(.secondary)
+                .padding(.horizontal)
+                .navigationTitle("Ingredients")
+            TextEditor (text: $steps)
+                .foregroundStyle(.secondary)
+                .padding(.horizontal)
+                .navigationTitle("Steps")
+            TextEditor (text: $chefsnotes)
+                .padding(.horizontal)
+                .navigationTitle("Chef's Notes")
+                }
+            }
+        }
+        struct View_3_Previews: PreviewProvider {
+            static var previews: some View {
+                View_3()
+            }
+        }
